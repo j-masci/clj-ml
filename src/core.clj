c807bf5e86994f916aa79d10bce44bf15874b6b3
Joel Home <masci.joel@gmail.com>
2020-02-27 12:03:08 -0500

    stuff

--- src/core.clj
+++ src/core.clj
@@ -8,10 +8,14 @@
 ; test scope
 (def a_variable 99)
 
-(defn br [] (print "\r\n"))
+; dotimes easier notation
+(defn _dotimes [times what] (dotimes [x times] (what)))
+
+(defn br ([] (print "\r\n"))
+         ([times] (_dotimes times br)))
 
 ; simple loop structure
-(defn func_2 [times] (dotimes [x times] (print "func 2. ")))
+(defn func_2 [times] (dotimes [x times] (print "func 2.")))
 
 ; alt def.
 (def func_3 (fn [p] (print "func_3..." p)))
@@ -31,6 +35,12 @@
                    ))
       )
 
+; ya doesn't work... not like this anyways.
+;(defn func_no_args [name thing] (def name (fn [] thing)))
+;(func_no_args blah (print "func no args blah"))
+;(blah)
+;(br)
+
 ; higher order
 (defn get_adder [amt] (fn [in] (+ in amt)))
 (def plus_5 (get_adder 5))
@@ -39,9 +49,15 @@
 (print (plus_5 100))
 (br)
 
+(defn funny#name!-_- [] (print "funny name"))
+
+(def my_list `(3 4 5))
+
+(print my_list)
 
 (defn -main []
       (do
+        (br 5)
         (print "main")
         (br)
         (other/func_from_another_namespace)
@@ -54,7 +70,9 @@
         (br)
         (variadic_func 1)
         (br)
-        (variadic_func 1 5 10)
+        ; (variadic_func 1 5 10)
+        (br)
+        (funny#name!-_-)
         (br)
         (func_in_core_ns_in_another_file)
       ))
